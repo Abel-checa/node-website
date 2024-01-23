8f9289a13ec62eac2be1f1fa106be0c23ab03c61
Abel Checa Rodríguez <abchkro@gmail.com>
2024-01-23 09:25:14 +0100

    Actualizacion

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.get('/',(req,res) =>{
-    res.send("<h2>Pagina principal</h2>")
-    res.end()
-});
+    const user = "<h1> USER: "+process.env.MYSQL_USER+"</h1>"
+    const host =  "<h1> HOST: "+process.env.MYSQL_HOST+"</h1>"
+    const pass =  "<h1>PASS: "+process.env.MYSQL_PASS+"</h1>"
 
+    res.send(user + host + pass)
+})
 
 app.get('/about',(req,res) =>{
     res.send("<h2>Acerca de</h2>")
     res.end()
 });
 
-app.listen(3000);
-console.log('El servidor arrancando en el puerto 3000;');
+console.log(process.env.MYSQL_HOST)
+
+app.listen(PORT);
+console.log('El servidor arrancando en el puerto', PORT);
 
 
 
